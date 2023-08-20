c362b5ac1560ac69674137276e32d0f52b0797d4
Nair0000 <myaccrainn@gmail.com>
2023-08-20 21:45:13 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // HTML Elements
 const textInput = document.getElementById('text-input');
-const speakBtn = document.getElementById('speak-btn');
 let displayStatus = document.getElementById('display-status');
 const ttsForm = document.getElementById('tts-form');
+const synth = window.speechSynthesis;
+const voiceSelect = document.getElementById('voiceSelect');
 
+const speakBtn = document.getElementById('speak-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const resumeBtn = document.getElementById('resume-btn');
 // Check web browser if speechSynthesis supported
 if ("speechSynthesis" in window) {
      displayStatus.innerText = 'Supported';
@@ -15,17 +19,56 @@ if ("speechSynthesis" in window) {
 ttsForm.addEventListener('submit', (event) => {
      handleOnSubmit(event);
 });
+pauseBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+     synth.pause();
+})
+resumeBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+     synth.resume();
+})
 
 // Functions
-function speak(text) {
-     const synth = window.speechSynthesis;
-     const utterance = new SpeechSynthesisUtterance(text);
-     synth.speak(utterance);
-     return;
-}
 function handleOnSubmit(event) {
-     event.preventDefault();
+     event.preventDefault(event);
      const text = textInput.value;
      speak(text);
      return;
 }
+
+function speak(text) {
+     const utterance = new SpeechSynthesisUtterance(text);
+     synth.speak(utterance);
+     return;
+}
+
+
+function populateVoiceList() {
+     if (typeof speechSynthesis === "undefined") {
+          return;
+     }
+
+     const voices = speechSynthesis.getVoices();
+
+     for (let i = 0; i < voices.length; i++) {
+          const option = document.createElement('option');
+          option.textContent = `${voices[i].name} - ${voices[i].lang}`;
+
+          if (voices[i].default) {
+               option.textContent += ' (Default)';
+          }
+
+          option.setAttribute('data-lang', voices[i].lang);
+          option.setAttribute('data-name', voices[i].name);
+          document.getElementById("voiceSelect").appendChild(option);
+     }
+}
+
+function onVoicesChanged() {
+     if (speechSynthesis.onvoiceschanged !== "undefined" && speechSynthesis.onvoiceschanged !== undefined) {
+          speechSynthesis.onvoiceschanged = populateVoiceList;
+     }
+}
+
+populateVoiceList();
+onVoicesChanged();
